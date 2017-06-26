d064d5a424901e15d8162aa9379a876382f99fe5
jeong <hellean@hanmail.net>
2017-06-26 13:44:12 +0900

    add create hello progreame

--- hello.java
+++ hello.java
@@ -2,25 +2,8 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Tell your World");
-	
+	system.out.println("Tell your world2");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 :q
 				
