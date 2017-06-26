2228d80db54a03a124277063d40f310904ef9631
jeong <hellean@hanmail.net>
2017-06-26 12:52:23 +0900

    add Tell his world

--- hello.java
+++ hello.java
@@ -2,25 +2,9 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Tell your World");
+		System.out.println("Tell his world");OB
 	
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 :q
 				
