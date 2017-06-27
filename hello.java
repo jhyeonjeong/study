27e385baae06d591940302ffd43a3d347e4ebc6d
jhyeonjeong <hellean@hanmail.net>
2017-06-27 09:11:50 +0900

    tell my world

--- hello.java
+++ hello.java
@@ -5,12 +5,11 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Tell your World");
-<<<<<<< HEAD
 		System.out.println("Tell his world");
 		system.out.println("Tell your world2");
-=======
-	system.out.println("Tell your world2");
->>>>>>> hotfix
+		system.out.println("Tell my world");
+
+
 	}
 }
 :q
